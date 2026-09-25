da6c65a80f13ea152f14cc7be58e6f7b7244c418
Sundar13905 <161055599+Sundar13905@users.noreply.github.com>
2026-09-25 16:04:26 +0530

    Update data_mem.v

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,29 +1,54 @@
 `timescale 1ns / 1ps
-//======================================================
-// Data Memory (16-bit wide, 256 words)
-//======================================================
+
 module data_mem (
-    input clk,
-    input mem_read,
-    input mem_write,
-    input [15:0] address,
-    input [15:0] write_data,
-    output reg [15:0] read_data
+
+    input  wire        clk,
+    input  wire        mem_read,
+    input  wire        mem_write,
+
+    input  wire [15:0] address,
+    input  wire [15:0] write_data,
+
+    output reg  [15:0] read_data
+
 );
+
     reg [15:0] memory [0:255];
+
     integer i;
+
     initial begin
+
         for (i = 0; i < 256; i = i + 1)
             memory[i] = 16'h0000;
+
     end
+
+
+    // ==========================================================
+    // Store
+    // ==========================================================
+
     always @(posedge clk) begin
+
         if (mem_write)
             memory[address[7:0]] <= write_data;
+
     end
+
+
+    // ==========================================================
+    // Load
+    // ==========================================================
+
     always @(*) begin
+
         if (mem_read)
             read_data = memory[address[7:0]];
+
         else
             read_data = 16'h0000;
+
     end
+
 endmodule
